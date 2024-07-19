c4d09a804df2a0a8a14a0c8e92d7a1c426f0113a
GB220 <gbedoyah@unal.edu.co>
2024-07-19 17:34:16 -0500

    Base del serializador/deserializador
    Se copia el contenido de las clases de serializador y deserializador en el Diseño Orientado a Objetos.

--- src/baseDatos/Deserializador.java
+++ src/baseDatos/Deserializador.java
@@ -1,5 +1,60 @@
 package baseDatos;
 
-public class Deserializador {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.List;
+import matricula.Departamento;
+import matricula.Asignatura;
+import matricula.Alumno;
 
+public class Deserializador {
+	// Este atributo es para definir la ruta al directoria temp que contiene las clases.
+	private static File rutaTemp = new File ("src\\baseDatos\\temp");
+	// Este método se encarga de cargar las listas de objectos que hay almacenados (serializados).
+	public static void deserializar (Departamento dpto) {
+		File [] docs = rutaTemp.listFiles();
+		FileInputStream fis;
+		ObjectInputStream ois;
+		
+		for (File file : docs) {
+			if (file.getAbsolutePath().contains("asignaturas")) {
+				try {
+					fis = new FileInputStream (file);
+					ois = new ObjectInputStream (fis);
+					
+					dpto.setAsignaturas((List<Asignatura>) ois.readObject());
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			} else if (file.getAbsolutePath().contains("alumnos")) {
+				try {
+					fis = new FileInputStream(file);
+					ois = new ObjectInputStream(fis);
+					
+					dpto.setAlumnos((List<Alumno>) ois.readObject());
+					
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
--- src/baseDatos/Serializador.java
+++ src/baseDatos/Serializador.java
@@ -1,5 +1,67 @@
 package baseDatos;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+
+import matricula.Departamento;
+
 public class Serializador {
+	private static File rutaTemp = new File ("src\\baseDatos\\temp");
+	
+	// Este método es el encargado de serializar las listas que están creadas en la clase SucursalCine.
+	public static void serializar(Departamento dpto) {
+		FileOutputStream fos;
+		ObjectOutputStream oos;
+		File [] docs = rutaTemp.listFiles();
+		PrintWriter pw;
+		
+		// Este método for borra el contenido de los archivos al momento de guardar los objetos para
+		// evitar que haya redundancia en los archivos y futuras complicaciones para buscar.
+		
+		for (File file : docs) {
+			try {
+				// Al crear este objeto PrintWriter y pasarle como parámetro, la ruta de cada
+				// archivo borra lo que haya en ellos automáticamente.
+				pw = new PrintWriter(file);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		for (File file : docs) {
+			
+			if (file.getAbsolutePath().contains("asignaturas")) {
+				try {
+					fos = new FileOutputStream(file);
+					oos = new ObjectOutputStream(fos);
+					oos.writeObject(dpto.getAsignaturas());
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			} else if (file.getAbsolutePath().contains("alumnos")) {
+				try {
+					fos = new FileOutputStream(file);
+					oos = new ObjectOutputStream(fos);
+					oos.writeObject(dpto.getAsignaturas());
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
 }
